000f0875b379f698a7bc53dbcd19383f241cd55f
Tigran <dijkstra.set@gmail.com>
2019-09-15 10:33:58 +0600

    almost full registration page

--- app/src/main/java/com/example/help_me/presentation/auth/registration/RegActivity.kt
+++ app/src/main/java/com/example/help_me/presentation/auth/registration/RegActivity.kt
@@ -2,6 +2,7 @@ package com.example.help_me.presentation.auth.registration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.help_me.R
 import kotlinx.android.synthetic.main.activity_reg.*
 
@@ -12,6 +13,19 @@ class RegActivity : AppCompatActivity() {
         setContentView(R.layout.activity_reg)
         toolbarRegBack.setOnClickListener {
             onBackPressed()
+        }
+
+        reg_radio_button.setOnCheckedChangeListener { reg_radio_button, i->
+            when (i) {
+                R.id.reg_radio_button_human -> {
+                    reg_const_company.visibility = View.GONE
+                    reg_const_person.visibility = View.VISIBLE
+                }
+                R.id.reg_radio_button_company -> {
+                    reg_const_company.visibility = View.VISIBLE
+                    reg_const_person.visibility = View.GONE
+                }
+            }
         }
     }
 }
