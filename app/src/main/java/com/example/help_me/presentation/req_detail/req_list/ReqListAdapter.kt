869bc7c085c387c938dbcdfa4de38d254a9d9d50
Tigran <dijkstra.set@gmail.com>
2019-11-23 19:40:40 +0600

    new upd

--- app/src/main/java/com/example/help_me/presentation/req_detail/req_list/ReqListAdapter.kt
+++ app/src/main/java/com/example/help_me/presentation/req_detail/req_list/ReqListAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.help_me.presentation.req_detail.req_list
 
 
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.help_me.R
+import com.example.help_me.entities.Company
+import com.example.help_me.entities.User
+import kotlinx.android.synthetic.main.item_company.view.*
+import kotlinx.android.synthetic.main.item_user.view.*
 
-class ReqListAdapter : RecyclerView.Adapter <ReqListAdapter.MyViewHolder>() {
-
-
+class ReqListAdapter : RecyclerView.Adapter <RecyclerView.ViewHolder>() {
     private var dataset = ArrayList<Any>()
     private lateinit var listener: OnItemClickListener
 
@@ -20,20 +26,56 @@ class ReqListAdapter : RecyclerView.Adapter <ReqListAdapter.MyViewHolder>() {
         notifyDataSetChanged()
     }
 
-
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
-        fun bind (item: Any) {
-x
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            1 -> {
+                UserViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_user, parent, false))
+            }
+            else -> {
+                CompanyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_company, parent ,false))
+            }
         }
-        init {
-            itemView.setOnClickListener(this)
-        }
+    }
 
+    override fun getItemViewType(position: Int): Int {
+        return when (dataset[position]) {
+            is User -> 1
+            else -> 2
+        }
+    }
+
+    override fun getItemCount(): Int = dataset.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (dataset[position]) {
+            is User -> {
+                (holder as UserViewHolder).bindView(dataset[position] as User)
+            }
+            else -> {
+                (holder as CompanyViewHolder).bindView(dataset[position] as Company)
+            }
+        }
     }
 
     interface OnItemClickListener {
         fun onItemClicked(item: Any, position: Int)
     }
 
+}
+
+class CompanyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    fun bindView(company: Company) {
+        itemView.item_company_title.text = company.title
+        itemView.city_item_company.text = company.title
+    }
+}
+
+class UserViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    fun bindView(user: User) {
+
+        itemView.item_user_namesurname.text = user.name + " " + user.surname
+        itemView.city_item_user.text = user.city
+    }
 }
--- app/src/main/java/com/example/help_me/presentation/req_detail/req_list/ReqListFragment.kt
+++ app/src/main/java/com/example/help_me/presentation/req_detail/req_list/ReqListFragment.kt
@@ -7,13 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.help_me.R
 import com.example.help_me.base.BaseFragment
+import com.example.help_me.entities.Company
 import com.example.help_me.entities.Req
+import com.example.help_me.entities.User
+import kotlinx.android.synthetic.main.fragment_req_list.*
 
 
-class ReqListFragment : BaseFragment() {
+class ReqListFragment : BaseFragment(), ReqListAdapter.OnItemClickListener {
 
     companion object {
         fun newInstance(data: Bundle? = null): ReqListFragment {
@@ -26,15 +30,28 @@ class ReqListFragment : BaseFragment() {
 
 
     lateinit var adapter: ReqListAdapter
-    private var reqList = ArrayList<Req>()
+    private var partList = ArrayList<Any>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         adapter = ReqListAdapter()
+        val list = ArrayList<Any>().apply {
+            add (User (name = "Aaaaa1", surname = "Bbbbb1", city = "Almaty"))
+            add (Company (title = "Aaaaa2", city = "Astana"))
+            add (User (name = "Aaaaa3", surname = "Bbbbb3", city = "Taraz"))
+            add (Company (title = "Aaaaa4", city = "Astana"))
+        }
         adapter.setListener(this)
+        adapter.setDataSet(list)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rq_lst_frt_rw.adapter = adapter
+        rq_lst_frt_rw.layoutManager = LinearLayoutManager(context)
+    }
 
-
+    override fun onItemClicked(item: Any, position: Int) {
+    }
 }
